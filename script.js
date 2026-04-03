773af4fb9ea2ff59154811ef612343a0ca240150
Murillo Invitation Studio <invitacionesdigitalesmuri@gmail.com>
2026-04-03 11:42:56 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// ========= Movimiento de slides de galeria ===============
+const slides = document.querySelector(".slides_galeria");
+const slideList = document.querySelectorAll(".slide");
+
+slideList.length
+
+let index = 0;
+
+function moverSlide(direccion){
+
+  index += direccion;
+
+  if(index < 0){
+    index = slideList.length - 1;
+  }
+
+  if(index >= slideList.length){
+    index = 0;
+  }
+
+  slides.style.transform = `translateX(-${index * 100}%)`;
+}
